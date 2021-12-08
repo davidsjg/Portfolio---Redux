e7b078b999f67e4f0df46aa8fef407ab02e294eb
Joey Davidson <j.davidson18@gmail.com>
2021-12-08 06:18:07 -0700

    predeploy commit

---
+++ client/src/firebase.js
@@ -0,0 +1,14 @@
+import {initializeApp} from firebase
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAG6bI_vx1J8-VVdzHXU2bQEAYy7ZNdDK8",
+    authDomain: "jgd-portfolio.firebaseapp.com",
+    projectId: "jgd-portfolio",
+    storageBucket: "jgd-portfolio.appspot.com",
+    messagingSenderId: "290035880738",
+    appId: "1:290035880738:web:9eba1a5a94d8fe61334b7b",
+    measurementId: "${config.measurementId}"
+  };
+
+  const app = initializeApp(firebaseConfig)
