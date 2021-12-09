fb66c422ec11727b5824a241976c4887c64f77a3
Joey Davidson <j.davidson18@gmail.com>
2021-12-09 13:39:17 -0700

    heroku deploy - auto-deploy turned off

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/portfolio" element={<Portfolio2 />} />
+            <Route path="/portfolio" element={<Portfolio />} />
           </Routes>
         </>
       </Router>
